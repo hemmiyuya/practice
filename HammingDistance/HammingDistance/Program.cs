17998d29b7b858364ad14b92af443f65b89aace2
DESKTOP-RSVPM06\ytenp <ytenpesto@gmail,com>
2021-04-13 13:31:46 +0900

    ハミング距離

---
+++ HammingDistance/HammingDistance/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace HammingDistance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(HammingDistanceNow(2, 4));
+        }
+
+        static int HammingDistanceNow(int x, int y)
+        {
+            //XOR
+            var z = x ^ y;
+            var count = 0;
+            //違う桁がなくなるまでカウント
+            while (z > 0)
+            {
+                count += z & 1;
+                z >>= 1;
+            }
+            return count;
+        }
+    }
+}
