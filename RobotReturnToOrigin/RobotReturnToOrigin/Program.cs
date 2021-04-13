95e1f03a22bd3085cec67e276f78fe0ccc4e100f
DESKTOP-RSVPM06\ytenp <ytenpesto@gmail,com>
2021-04-13 15:07:27 +0900

    RobotReturnToOrigin

---
+++ RobotReturnToOrigin/RobotReturnToOrigin/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+/// <summary>
+/// ロボットを移動させ、原点に戻ってきているかどうか判別する
+/// </summary>
+namespace RobotReturnToOrigin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string _sampleMove = "URL";
+            string _sampleMove2 = "UD";
+            string _sampleMove3 = "LR";
+            string _sampleMove4 = "LRLRLRLRLRLRLRLRUDUDUDUDUDUD";
+
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(RobotReturnOriginNow(_sampleMove));
+            Console.WriteLine(RobotReturnOriginNow(_sampleMove2));
+            Console.WriteLine(RobotReturnOriginNow(_sampleMove3));
+            Console.WriteLine(RobotReturnOriginNow(_sampleMove4));
+        }
+
+        /// <summary>
+        /// 移動と結果
+        /// </summary>
+        /// <param name="moves"></param>
+        /// <returns></returns>
+        static bool RobotReturnOriginNow(string moves)
+        {
+            bool l_Origin;
+            //縦の移動
+            int l_verticalLength = 0;
+            //横の移動
+            int l_sideLength = 0;
+
+            char[] movesArray = moves.ToCharArray();
+
+            for (int i = 0; i < moves.Length; i++)
+            {
+                switch (movesArray[i].ToString())
+                {
+                    case "U":
+                        l_verticalLength++;
+                        break;
+
+                    case "D":
+                        l_verticalLength--;
+                        break;
+
+                    case "R":
+                        l_sideLength++;
+                        break;
+
+                    case "L":
+                        l_sideLength--;
+                        break;
+                }
+            }
+
+            if (l_sideLength == 0 && l_verticalLength == 0)
+            {
+                l_Origin= true;
+            }
+            else
+            {
+                l_Origin= false;
+            }
+
+            return l_Origin;
+        }
+
+    }
+}
